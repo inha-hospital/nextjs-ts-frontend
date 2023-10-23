f722e4218e71ecba59768cfa2c38c624b68286a1
smlee111 <vic_sm@naver.com>
2023-10-23 17:55:47 +0900

    radix 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@radix-ui/themes/styles.css";
+// modules
+import type {AppProps} from "next/app";
+import {Theme, ThemePanel} from "@radix-ui/themes";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <Theme
+            appearance="dark"
+            accentColor="crimson"
+            grayColor="sand"
+            radius="large"
+            scaling="95%">
+            <Component {...pageProps} />
+            <ThemePanel />
+        </Theme>
+    );
 }
 
-export default MyApp
+export default MyApp;
