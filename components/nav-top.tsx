51d39cf955d30c7e55fa58ed790abbe55d3ec4b8
smlee111 <vic_sm@naver.com>
2023-12-26 22:10:12 +0900

    axios패키지 추가, nav추가

--- components/nav-top.tsx
+++ components/nav-top.tsx
@@ -1,8 +1,13 @@
-const NavTop = () => {
+import Link from "next/link";
+
+const Nav = () => {
     return (
         <>
             <div>네비게이션</div>
+            <Link href="/">홈</Link>
+            <Link href="/member/login">로그인</Link>
+            <Link href="/member/signup">회원가입</Link>
         </>
     );
 };
-export default NavTop;
+export default Nav;
