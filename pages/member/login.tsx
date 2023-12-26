25e44f53e4b7e15fda0757aaba2c7e1f541411c8
c9u11 <tjrdud6412@naver.com>
2023-12-26 23:14:35 +0900

    🚧 Login Page
    - Form 사용

--- pages/member/login.tsx
+++ pages/member/login.tsx
@@ -1,13 +1,41 @@
-import { Box, Button } from "@radix-ui/themes";
+import { Box, Button, Heading, TextField } from "@radix-ui/themes";
+import * as Form from "@radix-ui/react-form";
 
-const Login = () => {
-    return (
-        <>
-            <div>로그인</div>
-            <Box mx="auto" style={{ width: "300px" }}>
-                <Button style={{ width: "100%" }}>버튼입니당</Button>
-            </Box>
-        </>
-    );
+const login = () => {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+  }
+  return (
+    <Box>
+      <Heading size='6'>로그인</Heading>
+      <Form.Root onSubmit={onSubmit}>
+        <Form.Field name="email">
+          <Form.Message match="valueMissing" color="red">
+            이메일을 입력해주세요.
+          </Form.Message>
+          <Form.Message match="typeMismatch">
+            이메일 형식이 아닙니다.
+          </Form.Message>
+          <Form.Control asChild>
+            <TextField.Input type="email" required placeholder="이메일" />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field name="password">
+          <Form.Message match="valueMissing">
+            비밀번호를 입력해주세요.
+          </Form.Message>
+          <Form.Control asChild>
+            <TextField.Input type="password" required placeholder="비밀번호" />
+          </Form.Control>
+        </Form.Field>
+        <Form.Submit asChild>
+          <Button type="submit" >Submit</Button>
+        </Form.Submit>
+      </Form.Root>
+    </Box>
+  );
 };
-export default Login;
+export default login;
