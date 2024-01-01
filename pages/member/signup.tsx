8d7ea46e85188d0a683410da04f026dfcf9c9376
c9u11 <tjrdud6412@naver.com>
2024-01-02 00:24:11 +0900

    🚧 Signup Page
    - Form 사용

--- pages/member/signup.tsx
+++ pages/member/signup.tsx
@@ -1,8 +1,38 @@
+import { Box, Heading } from "@radix-ui/themes";
+import { Root, Field, Message, Submit } from "components/form";
+
 const Signup = () => {
-    return (
-        <>
-            <div>회원가입</div>
-        </>
-    );
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+  }
+  return (
+    <Box>
+      <Heading size='6'>회원가입</Heading>
+      <Root onSubmit={onSubmit}>
+        <Field name="이름" required placeholder="이름">
+          <Message match="valueMissing" message="이름을 입력해주세요." />
+        </Field>
+        <Field name="생년월일" type="number" required placeholder="생년월일">
+          <Message match="valueMissing" message="생년월일을 입력해주세요." />
+        </Field>
+        <Field name="전화번호" type="number" required placeholder="전화번호">
+          <Message match="valueMissing" message="전화번호를 입력해주세요." />
+        </Field>
+        <Field name="이메일" type="email" required placeholder="이메일">
+          <>
+            <Message match="valueMissing" message="이메일을 입력해주세요." />
+            <Message match="typeMismatch" message="이메일 형식이 아닙니다." />
+          </>
+        </Field>
+        <Field name="비밀번호" type="password" required placeholder="비밀번호">
+          <Message match="valueMissing" message="비밀번호를 입력해주세요." />
+        </Field>
+        <Submit text="로그인" />
+      </Root>
+    </Box>
+  );
 };
 export default Signup;
