266ae74263a52acf393d20739ea21c7594664084
c9u11 <tjrdud6412@naver.com>
2024-01-01 23:27:17 +0900

    💄 경고 메시지 디자인 변경
    - 글씨 색상 빨간색으로 변경

--- pages/member/login.tsx
+++ pages/member/login.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Heading, TextField } from "@radix-ui/themes";
+import { Box, Button, Heading, TextField, Text } from "@radix-ui/themes";
 import * as Form from "@radix-ui/react-form";
 
 const login = () => {
@@ -13,19 +13,19 @@ const login = () => {
       <Heading size='6'>로그인</Heading>
       <Form.Root onSubmit={onSubmit}>
         <Form.Field name="email">
-          <Form.Message match="valueMissing" color="red">
-            이메일을 입력해주세요.
+          <Form.Message match="valueMissing" asChild>
+            <Text color="red">이메일을 입력해주세요.</Text>
           </Form.Message>
-          <Form.Message match="typeMismatch">
-            이메일 형식이 아닙니다.
+          <Form.Message match="typeMismatch" asChild>
+            <Text color="red">이메일 형식이 아닙니다.</Text>
           </Form.Message>
           <Form.Control asChild>
             <TextField.Input type="email" required placeholder="이메일" />
           </Form.Control>
         </Form.Field>
         <Form.Field name="password">
-          <Form.Message match="valueMissing">
-            비밀번호를 입력해주세요.
+          <Form.Message match="valueMissing" asChild>
+            <Text color="red">비밀번호를 입력해주세요.</Text>
           </Form.Message>
           <Form.Control asChild>
             <TextField.Input type="password" required placeholder="비밀번호" />
