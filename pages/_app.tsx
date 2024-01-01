c0428ed728d5f7af273ad4c03abf475e0b6fab3f
c9u11 <tjrdud6412@naver.com>
2024-01-02 01:16:46 +0900

    ✨ Recoil
    - Recoil을 사용하여 Counter 예제 개발

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,24 @@ import "@radix-ui/themes/styles.css";
 import type { AppProps } from "next/app";
 import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavTop from "components/nav-top";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Theme
-            appearance="dark"
-            accentColor="crimson"
-            grayColor="sand"
-            radius="large"
-            scaling="95%"
-        >
-            <NavTop />
-            <Component {...pageProps} />
-            {/* <ThemePanel /> */}
-        </Theme>
-    );
+  return (
+    <RecoilRoot>
+      <Theme
+        appearance="dark"
+        accentColor="crimson"
+        grayColor="sand"
+        radius="large"
+        scaling="95%"
+      >
+        <NavTop />
+        <Component {...pageProps} />
+        {/* <ThemePanel /> */}
+      </Theme>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
