90e6916bcd4fe5581dbf25b46b96e197d2097d1d
c9u11 <tjrdud6412@naver.com>
2024-01-23 20:37:21 +0900

    ✨ apiClient
    - request시 token 값을 자동으로 header에 넣는 기능 개발
    - response에 따라 에러 처리를 할 수 있도록 분기 처리

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const apiClient = axios.create();
+
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
